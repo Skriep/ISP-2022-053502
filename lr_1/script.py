73ed11dd857e609da084879ca88e470efe7363bc
Skriep <skriepcoder@gmail.com>
2022-02-22 21:14:02 +0300

    Change Docstrings
    Add Docstrings to all functions.
    Describe all functions in module's Docstring.

--- lr_1/script.py
+++ lr_1/script.py
@@ -8,13 +8,18 @@ This script can calculate the following parameters for an arbitrary text:
 - top-K most frequent N-grams with default values of K = 10 and N = 4.
 
 If imported as a module, the following functions can be used:
-- get_input - returns values entered by the user: N, K, text
-- get_words_in_sentences - splits the text into words grouped by sentences
-- get_word_frequencies - returns all words' frequencies
-- get_average_words_in_sentence - returns average amount of words in a sentence
-- get_median_words_in_sentence - returns median amount of words in a sentence
-- get_all_ngrams - returns all the n-grams
-- main - the main function
+- get_input - returns values entered by the user: N, K, text;
+- get_words_in_sentences - splits the text into words grouped by sentences;
+- get_word_frequencies - returns all words' frequencies;
+- get_average_words_in_sentence - returns average amount
+of words in a sentence;
+- get_median_words_in_sentence - returns median amount of words in a sentence;
+- get_all_ngrams - returns all the n-grams;
+- main - the main function.
+
+The following global variables are used:
+- K_DEFAULT - the default value for K (used in function get_input);
+- N_DEFAULT - the default value for N (used in function get_input).
 """
 import string
 
@@ -23,6 +28,19 @@ N_DEFAULT = 4
 
 
 def get_input() -> 'tuple[int, int, str]':
+    """
+    Return a tuple of values K, N, text.
+
+    Ask the user to enter values of K, N, text.
+    K must be a non-negative integer.
+    N must be an integer greater than 0.
+    text may be an arbitrary string.
+
+    If some of the values K, N are entered incorrectly,
+    the default values are used.
+    Default values for K, N must be stored in global variables
+    K_DEFAULT and N_DEFAULT respectively.
+    """
     k_string = input('Enter K (the number of top N-grams to be printed) '
                      f'or nothing for the default value of {K_DEFAULT}: ')
     if k_string.isnumeric():
@@ -53,14 +71,14 @@ def get_words_in_sentences(text: str) -> 'list[list[str]]':
     """
     Return lists of words grouped into sentences.
 
-    If the text is an empty string, return a list containing an empty list: \
+    If the text is an empty string, return a list containing an empty list:
     [[]];
-    Otherwise, return a list in the following form: \
+    Otherwise, return a list in the following form:
     [['word', 'word'], ['word'], ...].
 
-    If any word ends in '.', '!' or '?', \
+    If any word ends in '.', '!' or '?',
     it is considered the end of a sentence.
-    All trailing punctuation (as defined in string.punctuation) \
+    All trailing punctuation (as defined in string.punctuation)
     in words is stripped.
     Leading characters '"', "'", '(', '{', '[' in words are also stripped.
     """
@@ -79,6 +97,14 @@ def get_words_in_sentences(text: str) -> 'list[list[str]]':
 
 
 def get_word_frequencies(sentences: 'list[list[str]]') -> 'dict[str, int]':
+    """
+    Return a dictionary of word frequencies of occurrence in the text.
+
+    Dictionary's keys are words (strings),
+    and values are their frequencies of occurence.
+
+    The text must be split into lists of words grouped into sentences.
+    """
     word_frequencies = {}
     for sentence in sentences:
         for word in sentence:
@@ -90,6 +116,13 @@ def get_word_frequencies(sentences: 'list[list[str]]') -> 'dict[str, int]':
 
 
 def get_average_words_in_sentence(sentences: 'list[list[str]]') -> float:
+    """
+    Return an average amount of words in a sentence of the text.
+
+    If text contains no words, return 0.
+
+    The text must be split into lists of words grouped into sentences.
+    """
     sentence_word_counts = {len(sentence) for sentence in sentences}
     if len(sentence_word_counts) == 0:
         return 0
@@ -98,6 +131,18 @@ def get_average_words_in_sentence(sentences: 'list[list[str]]') -> float:
 
 
 def get_median_words_in_sentence(sentences: 'list[list[str]]') -> float:
+    """
+    Return a median amount of words in a sentence of the text.
+
+    If text contains no words, return 0.
+    If text contains an odd amount of words,
+    return the middle value in the center of the list of sentences' lengths.
+    If text contains an even amount of words,
+    return an average of the two middle values in the center
+    of the list of sentences' lengths.
+
+    The text must be split into lists of words grouped into sentences.
+    """
     sentence_word_counts = sorted([len(sentence) for sentence in sentences])
     word_counts_len = len(sentence_word_counts)
     if word_counts_len == 0:
@@ -114,6 +159,14 @@ def get_median_words_in_sentence(sentences: 'list[list[str]]') -> float:
 
 
 def get_all_ngrams(sentences: 'list[list[str]]', n) -> 'dict[str, int]':
+    """
+    Return a dictionary with all n-grams and their frequencies in the text.
+
+    Dictionary's keys are words (n-grams),
+    and values are their frequencies of occurence in text.
+
+    The text must be split into lists of words grouped into sentences.
+    """
     n_grams: dict[str, int] = {}
     for sentence in sentences:
         for word in sentence:
@@ -129,6 +182,17 @@ def get_all_ngrams(sentences: 'list[list[str]]', n) -> 'dict[str, int]':
 
 
 def main():
+    """
+    Print some information about the text entered by the user.
+
+    First, the function get_input() is called.
+    Then, the entered text is processed
+    and the following information is printed:
+    - how many times does each word repeat in the text;
+    - average amount of words in a sentence;
+    - median amount of words in a sentence;
+    - top-K most frequent N-grams and their frequencies of occurence.
+    """
     k, n, text = get_input()
     sentences = get_words_in_sentences(text)
 
