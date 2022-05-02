f9feaf6de7b88e5be1b824631ee1a9f00685d245
Skriep <skriepcoder@gmail.com>
2022-05-02 22:41:33 +0300

    Add tests

--- lr_2/tests/test_packer.py
+++ lr_2/tests/test_packer.py
@@ -1,14 +1,20 @@
 from myserializer.packer import Packer
+import pytest
 
 
-def test_dict():
-    mydict = {
-        'key': 'value',
-        123: 456,
-        12.3: 4.56,
-        1.4j + 3: "that's complex!",
-        (1, 2, 3): (4, 5, 6)
-    }
-    packed = Packer.pack(mydict)
+def pack_unpack(item):
+    packed = Packer.pack(item)
     unpacked = Packer.unpack(packed)
-    assert all(unpacked[key] == value for key, value in mydict.items())
+    return unpacked
+
+
+@pytest.mark.parametrize('test_input', [
+    1, 2, 12, 13434, -134134, 0, 7,
+    True, False, (False, True), [True, (1, 2, 'k9')],
+    {1: 2}, {'a': 'b'}, {None: 'ok'},
+    {'key': 'value', 123: 456, 12.3: 4.56, (1.4j + 3): "that's complex!",
+     (1, 2, 3): (4, 5, 6)},
+    {'deep': {'deep': {'deep': {'deep': {'deep': {'deep': {'deep': 1}}}}}}}
+])
+def test_packing(test_input):
+    assert pack_unpack(test_input) == test_input
