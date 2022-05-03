f2e2ce006e56cb7c7e12a569792ac9e1f49ff63d
Skriep <skriepcoder@gmail.com>
2022-05-03 20:00:13 +0300

    Cover myserializer exceptions in tests

--- lr_2/tests/serialization_options.py
+++ lr_2/tests/serialization_options.py
@@ -1,6 +1,12 @@
 import myserializer
 
-
-serializers = list(map(myserializer.create_serializer, [
+implemented_types = [
     'yaml', 'toml', 'json'
-]))
+]
+
+serializers = list(map(myserializer.create_serializer,
+                       implemented_types))
+
+not_implemented_types = [
+    'xml'
+]
---
+++ lr_2/tests/test_myserializer.py
@@ -0,0 +1,34 @@
+from myserializer import create_serializer
+from serialization_options import implemented_types as implemented
+from serialization_options import not_implemented_types as not_implemented
+import pytest
+from typing import List
+import random
+
+
+def randomize_uppercase(lst: List[str]):
+    result = []
+    random.seed(0)
+    for item in lst:
+        pos = random.randint(0, len(item) - 1)
+        modified = ''
+        if pos != 0:
+            modified += item[:pos]
+        modified += item[pos].upper()
+        if pos < len(item):
+            modified += item[pos+1:]
+        result.append(modified)
+    return result
+
+
+@pytest.mark.parametrize('serializer_type',
+                         implemented + randomize_uppercase(implemented))
+def test_implemented(serializer_type):
+    assert create_serializer(serializer_type) is not None
+
+
+@pytest.mark.parametrize(
+    'serializer_type', not_implemented + randomize_uppercase(not_implemented))
+def test_not_implemented(serializer_type):
+    with pytest.raises(NotImplementedError):
+        create_serializer(serializer_type)
