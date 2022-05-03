dca70b3e2c3a40ee07874214ed87d557d4b19665
Skriep <skriepcoder@gmail.com>
2022-05-03 19:39:32 +0300

    Add more JSON tests

--- lr_2/tests/encoding_inputs.py
+++ lr_2/tests/encoding_inputs.py
@@ -1,12 +1,16 @@
 
 _test_strings: list = [
-    'abcdefg', 'with new\r\nlines', '\b\f\n\r\t \v\a',
+    'abcdefg', 'with new\r\nlines', '\b\f\n\r\t \v\a', '',
     'unicode \u0014\u0ca7\u1337', '"\'!@@^*&^!@($!*@$)!@\'"'
 ]
 
 _test_dicts: list = [
     {'test123': '123'}, {'12345': '27', '123': '45', '12': ['1', '2']},
-    {'taet': ['123', '234', {'12\n': '123', '3': '4'}], 'f': '3'},
+    {'taet': ['123', '234', {'12\n': '123', '3': '4'}], 'f': '3'}, {}
 ]
 
-test_inputs: list = _test_strings + _test_dicts
+_test_lists: list = [
+    ['1'], ['1', '2', '3'], ['\x12', '2', ['1', '', {}], {'1': ['2', '']}]
+]
+
+test_inputs: list = _test_strings + _test_dicts + _test_lists
